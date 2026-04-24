4bae4aed0a4a9ae056201c5d1f0be6f43b077f57
Nathan MG <nathanmareyguidi@gmail.com>
2026-04-24 17:44:03 +0200

    refactor(airflow): ticker list now in config folder

---
+++ airflow/config/ticker.py
@@ -0,0 +1,42 @@
+tickerStrings = [
+    "AC.PA",    # Accor
+    "ADP.PA",   # Aéroports de Paris
+    "AIR.PA",   # Airbus
+    "ALLO.PA",  # Alstom
+    "MT.PA",    # ArcelorMittal
+    "MC.PA",    # LVMH/Arnault
+    "CS.PA",    # AXA
+    "BNP.PA",   # BNP Paribas
+    "EN.PA",    # Bouygues
+    "BVI.PA",   # Bureau Veritas
+    "CAP.PA",   # Capgemini
+    "CA.PA",    # Carrefour
+    "ACA.PA",   # Crédit Agricole
+    "DSY.PA",   # Dassault Systèmes
+    "DANON.PA", # Danone
+    "EDF.PA",   # EDF
+    "ENGI.PA",  # Engie
+    "EL.PA",    # Essilor Luxottica
+    "ERA.PA",   # Eramet
+    "ERF.PA",   # Eurofins
+    "GET.PA",   # Getlink
+    "RMS.PA",   # Hermès
+    "IPN.PA",   # Ipsen
+    "KER.PA",   # Kering
+    "LI.PA",    # Klepierre
+    "LR.PA",    # Legrand
+    "OR.PA",    # L'Oréal
+    "ML.PA",    # Michelin
+    "ORAN.PA",  # Orange
+    "RI.PA",    # Pernod Ricard
+    "PUB.PA",   # Publicis
+    "RNO.PA",   # Renault   
+    "RXL.PA",   # Rexel
+    "SAF.PA",   # Safran
+    "SGO.PA",   # Saint-Gobain
+    "SNPI.PA",  # Sanofi
+    "SU.PA",    # Schneider Electric
+    "GLE.PA",   # Société Générale
+    "SOD.PA",   # Sodexo
+    "STM.PA",   # STMicroelectronics
+]

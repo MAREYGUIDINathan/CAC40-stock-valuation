76fd90dd8d98b6f216df99d7052083b8710c1d03
Nathan MG <nathanmareyguidi@gmail.com>
2026-04-25 10:13:56 +0200

    fix(api): change table name

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -41,7 +41,7 @@ def get_ticker(period: str, ticker: str = "ENGI.PA"):
         result = connection.execute(
             text("""
                 SELECT "Date", "Open", "High", "Low", "Close", "Volume", "Ticker"
-                FROM market_data.daily_prices
+                FROM market_data.market_prices
                 WHERE "Ticker" = :ticker
                   AND "Date" BETWEEN :start_date AND :end_date
                 ORDER BY "Date" ASC

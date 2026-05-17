157ccdbd5201d649a1d0d883bbb1baccdf5cd575
Nathan MG <nathanmareyguidi@gmail.com>
2026-05-17 13:49:14 +0200

    feat(streamlit): major change front end

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,6 +7,11 @@ from datetime import datetime, timedelta
 
 API = os.getenv("API_BASE_URL", "http://fastapi:8000")
 
+st.set_page_config(
+    page_title="Valorisation des actions du CAC 40",
+    page_icon="💰",
+    layout="wide"
+)
 
 @st.cache_data(ttl=60)
 def create_df(period: str = "5y", ticker: str = "ENGI.PA") -> pd.DataFrame:
@@ -20,47 +25,10 @@ def create_df(period: str = "5y", ticker: str = "ENGI.PA") -> pd.DataFrame:
 def get_tickers_options() -> list:
     """Récupère la liste des tickers disponibles."""
     return httpx.get(f"{API}/tickers").json()["tickers"]
-
-
-def init_session_state():
-    """Initialise les variables du session_state."""
-    defaults = {
-        "ticker_selected": "ENGI.PA",
-        "period_filter": "5y"
-    }
     
     for key, value in defaults.items():
         if key not in st.session_state:
             st.session_state[key] = value
-
-
-def create_sync_ticker_selectbox(label: str, key: str):
-    """Crée un selectbox synchronized automatiquement avec tous les autres ticker_selectbox.
-    
-    Tous les selectbox avec une clé commençant par 'ticker_select_' seront synchronisés automatiquement.
-    Aucune configuration supplémentaire n'est nécessaire.
-    """
-    options = get_tickers_options()
-    
-    # Initialise la clé si elle n'existe pas
-    if key not in st.session_state:
-        st.session_state[key] = "ENGI.PA"
-    
-    def sync_callback():
-        selected_value = st.session_state[key]
-        st.session_state["ticker_selected"] = selected_value
-        # Sync tous les autres selectbox ticker automatiquement
-        for session_key in st.session_state.keys():
-            if session_key.startswith("ticker_select_") and session_key != key:
-                st.session_state[session_key] = selected_value
-    
-    st.selectbox(
-        label,
-        options=options,
-        key=key,
-        on_change=sync_callback
-    )
-
 
 def line_chart(data_filtered: pd.DataFrame) -> None:
     # Affichage d'un graphique centré sur la moyenne avec une échelle pertinente (Close vs. Datetime)
@@ -133,130 +101,18 @@ def line_chart(data_filtered: pd.DataFrame) -> None:
 
     st.altair_chart(line_chart, width="stretch")
 
+with st.sidebar:
+    st.header("val.cac40")
+
+    st.session_state["ticker_selected"]  = st.pills("ENTREPRISES", get_tickers_options(), selection_mode="single", required=True, default=get_tickers_options()[0])
+    
+    st.session_state["period_filter"] = st.pills("PÉRIODE", ["1M", "6M", "CY", "1Y", "5Y"], selection_mode="single", required=True, default="5Y")
+
+    st.pills("RATIO", ["P/E Ratio", "P/S Ratio", "Dividend Yield"], selection_mode="single", required=True, default="P/E Ratio")
+
 
 # Show Title
-st.title("Introduction to Stock Market", text_alignment="center")
-
-# -------------------------------------
-#   Sommaire
-# -------------------------------------
-
-# -------------------------------------
-#   Introduction
-# -------------------------------------
-
-st.subheader("Introduction")
-st.write("""
-Bienvenue dans cette application d'introduction à la bourse !  
-
-Ici, vous pouvez explorer les données de marché de différentes actions, comprendre les concepts clés de la bourse, et apprendre comment les investisseurs prennent des décisions basées sur les données.  
-
-Que vous soyez un débutant ou que vous souhaitez approfondir vos connaissances, cette application est conçue pour vous !   
-""")
-
-# -------------------------------------
-#   Les Fondamentaux de la Bourse
-# -------------------------------------
-
-st.subheader("I - Les Fondamentaux de la Bourse")
-
-st.write("""
-La bourse est un marché où s’échangent des produits financiers, appelés « instruments financiers », ou valeurs mobilières.  
- 
-Les différents instruments financiers sont :  
-
-- les **actions** (titres de propriété d'une partie du capital d'une entreprise), 
-- les **obligations** (prêt d'argent contractuel à un organisme public ou privé), 
-- les **parts d'OPCVM** (Organismes de Placement Collectif en Valeurs Mobilières).  
-
-Devenir propriétaire d'une valeurs mobilières donne accèes à des droits: 
-""")
-col1, col2, col3 = st.columns(3)
-with col1:
-    with st.container(border=True):
-        st.write("""
-        ##### Action
-        - Dividende
-        - Vote aux assemblées des actionnaires
-        """)
-with col2:
-    with st.container(border=True):
-        st.write("""
-        ##### Obligation
-        - Intérêt versé chaque années
-        - Remboursement à la fin du contrat
-        """)
-with col3:
-    with st.container(border=True):
-        st.write("""
-        ##### OPCVM
-        - Propre à chaque OPCVM
-        """)
-        
-st.write("""
-Comme sur tous les marchés, le prix dépend de l’offre et de la demande. Si l’offre est supérieure à la demande, le prix diminue pour atteindre l’équilibre.  
-À l’inverse, quand la demande est supérieure à l’offre, le prix augmente pour atteindre l’équilibre.  
-
-La fonction première de la Bourse est de permettre aux investisseurs d’acheter et de vendre leurs titres sur le marché secondaire. C’est ce qu’on appelle la liquidité(marché sur lequel ont lieu beaucoup de transactions.)
-""")
-
-# -------------------------------------
-# II - Comprendre ce qu'on achète
-# -------------------------------------
-
-st.subheader("II - Comprendre ce qu'on achète")
-col1, col2 = st.columns(2, border=True)
-with col1:
-    st.markdown("""##### Action""")
-    st.write("""
-    Une action représente une part de propriété dans une entreprise.  
-
-    En achetant une action, vous devenez actionnaire de cette entreprise et vous avez droit à une partie de ses bénéfices, ainsi qu'à une voix lors des assemblées générales.  
-
-    Il est important de faire des recherches sur l'entreprise avant d'investir.  
-    Cela inclut la compréhension de son modèle économique, de sa position sur le marché, de sa santé financière, et de ses perspectives de croissance.  
-    """)
-with col2:
-    st.markdown("""##### Valeur vs. Prix""")
-    st.write("""
-    Le prix d'une action est simplement le montant que les investisseurs sont prêts à payer pour une part de l'entreprise à un moment donné.  
-    
-    La valeur de l'entreprise, en revanche, est une estimation de ce que l'entreprise vaut réellement, basée sur ses actifs, ses revenus, sa croissance potentielle, et d'autres facteurs fondamentaux.  
-    
-    Il est possible qu'une action soit surévaluée (prix élevé par rapport à la valeur) ou sous-évaluée (prix bas par rapport à la valeur).
-    """)
-
-
-# -------------------------------------
-#  III - Les Données Essentielles
-# -------------------------------------
-
-# Show subtitle
-st.subheader(
-    "III - Les Données Essentielles",
-)
-
-init_session_state()
-
-create_sync_ticker_selectbox("Sélectionnez une action", "ticker_select_1")
-
-st.write(f"Cours de cloture de : **{st.session_state['ticker_selected']}**")
-
-if "period_filter" not in st.session_state:
-    st.session_state["period_filter"] = "5y"
-
-# Show button
-with st.container(horizontal=True, horizontal_alignment="left"):
-    if st.button("1M", type="secondary"):
-        st.session_state["period_filter"] = "1m"
-    if st.button("6M"):
-        st.session_state["period_filter"] = "6m"
-    if st.button("CY"):
-        st.session_state["period_filter"] = "CY"
-    if st.button("1Y"):
-        st.session_state["period_filter"] = "1y"
-    if st.button("5Y"):
-        st.session_state["period_filter"] = "5y"
+st.title("Valorisation des actions du CAC 40", text_alignment="left")
 
 data = create_df(st.session_state["period_filter"], st.session_state["ticker_selected"])
 
@@ -335,10 +191,6 @@ with col2:
 # IV - Valorisation de l'Entreprise
 # -------------------------------------
 
-st.subheader("IV - Valorisation de l'Entreprise")
-
-create_sync_ticker_selectbox("Sélectionnez une action", "ticker_select_2")
-
 col1, col2, col3 = st.columns(3)
 with col1:
     st.popover("P/E Ratio", icon="❓").write("""
@@ -364,19 +216,6 @@ with col3:
     **Calcul**  
     Dividend Yield = (Dividende par action / Prix de l'action) * 100
     """)
-# -------------------------------------
-#  V - Santé Financière
-# -------------------------------------
-st.subheader("V - Santé Financière")
-create_sync_ticker_selectbox("Sélectionnez une action", "ticker_select_3")
-# -------------------------------------
-#  VI - Analyse Stratégique
-# -------------------------------------
-st.subheader("VI - Analyse Stratégique")
-create_sync_ticker_selectbox("Sélectionnez une action", "ticker_select_4")
-# -------------------------------------
-#  Bonus - Explorer les données
-# -------------------------------------
 
 # -------------------------------------
 #  Footer
